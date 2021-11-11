652e145fefb2df0260c72eab541b3516989fb3ac
ChandraBChoudhary <94103997+ChandraBChoudhary@users.noreply.github.com>
2021-11-11 13:20:05 +0530

    My python files

---
+++ Fibo.py
@@ -0,0 +1,27 @@
+'''Fibonacci series '''
+num=0
+num1=1
+print("Fibonacii series are : ")
+print(num)
+print(num1)
+counter=0
+for x in range(8) :
+    fibo=num+num1
+    print(fibo)
+    num=num1
+    num1=fibo
+    
+
+''' First 10 even fibonacci series'''
+
+num=0
+num1=1
+print("First 10 even Fibonacii series are : ")
+print(num)
+counter=0
+for x in range(30) :
+    fibo=num+num1
+    if (fibo%2==0):
+        print(fibo)
+    num=num1
+    num1=fibo
